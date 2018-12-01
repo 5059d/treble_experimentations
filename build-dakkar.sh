cf2badd60c4f7942569589db38c2e23770f4a233
Milan Kragujević <milankragujevic@gmail.com>
2018-12-01 12:28:31 +0100

    Update build-dakkar.sh

--- build-dakkar.sh
+++ build-dakkar.sh
@@ -285,7 +285,7 @@ function clone_or_checkout() {
             git checkout origin/"$localManifestBranch"
         )
     else
-        git clone https://github.com/phhusson/"$repo" "$dir" -b "$localManifestBranch"
+        git clone https://github.com/5059d/"$repo" "$dir" -b "$localManifestBranch"
     fi
 }
 
